f783904b34a02732023c9fef61c847981d18542a
RAJESH <bigdatacloudsolution@gmail.com>
2024-07-28 01:49:08 +0530

    updated all files

--- LearnBe4/scripts/scripts.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("JavaScript is loaded and running!");
-    
-    // Add additional JavaScript interactions if needed
-});
---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("JavaScript is loaded and running!");
+    
+    // Add additional JavaScript interactions if needed
+});
