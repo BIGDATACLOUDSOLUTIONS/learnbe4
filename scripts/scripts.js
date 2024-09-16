af4b35e117d43cfe1b0a640040148c124ba3fc56
Narayan3110 <narayanpandey92200@gmail.com>
2024-09-16 17:32:42 +0530

    topic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("JavaScript is loaded and running!");
-    
-    // Add additional JavaScript interactions if needed
+    const image = document.getElementById('animated-image');
+
+    // function animateImage() {
+    //     image.style.transform = 'scale(1.1)';
+    //     image.style.opacity = '0.9';
+
+    //     setTimeout(() => {
+    //         image.style.transform = 'scale(1)';
+    //         image.style.opacity = '1';
+    //     }, 1000);
+    // }
+
+    // // Run animation every 2 seconds
+    // setInterval(animateImage, 2000);
 });
